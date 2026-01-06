808222a58db9a7a78465d3f5e42307ff3d0d35a4
Sinxn-coder <msinankavala786@gmail.com>
2026-01-06 17:58:00 +0530

    Update sculpted wave bottom navigation bar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import BottomNav from '../components/BottomNav';
 import FoodParticles from '../components/FoodParticles';
 import SetUsernameModal from '../components/SetUsernameModal';
@@ -7,6 +7,17 @@ import SetAvatarModal from '../components/SetAvatarModal';
 import AppBar from '../components/AppBar';
 
 const MainLayout = ({ lang }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Scroll to top when route changes
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }, [location.pathname]);
+
     return (
         <div style={{ minHeight: '100vh', background: 'var(--bg-cream)', position: 'relative' }}>
             {/* Top Navigation Bar */}
